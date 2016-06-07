3a30470b4fc4fb4a27bee817962cf7fbd1db3c16
Dan Caciula <dan.caciula@reea.net>
2016-06-07 15:14:00 +0300

    project create

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+         <script src="main.js"></script>
+         <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <div>caciula</div>
+    </body>
+
+</html>
